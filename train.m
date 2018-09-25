c46940f3e142bd314b2353c49a27a86c67575b0d
prodrigu <G2014084649h>
2018-09-25 00:25:39 -0600

    train with error

--- train.m
+++ train.m
@@ -3,33 +3,21 @@ function [W1,W2]=train(W1,W2,X,Y,lambda,batchSize)
     threshold = 0.00005;
     counter = 0;
 
-
-    if(columns(X)<3)
-        X=[ones(rows(X),1),X];
-    endif;
-
+    X=[ones(rows(X),1),X];
+    w=packweight(W1,W2);
 
     W1size=size(W1);
     W2size=size(W2);
+    XY = [X,Y];
+    XY_samples = XY(randi(length(XY),batchSize,1),:)
 
-    w=packweight(W1,W2);
-
-
-    #gradientes conjugados
-    [r,p]=unpackweight(w,W1size,W2size);
-    ff= @(w) target(r,p,X,Y); #funcion de error
-    df= @(w) gradtarget(r,p,X,Y); #funcion de gradiente
-
-    [w2,v,nev]=cg_min(df,ff,w); #funcion de gradientes conjugados
-
-    #numero de evaluaciones 
-    nEva=nev(1,1) #numero de evaluaciones de la función de error
-
+    X_samples = XY_samples(:,1:columns(X))
+    Y_samples = XY_samples(:,columns(X)+1:end)
     #Backpropagation
     do
         counter=counter+1;
-        J1=target(W1,W2,X,Y);
-        [gW1,gW2]=gradtarget(W1,W2,X,Y);
+        J1=target(W1,W2,X_samples,Y_samples);
+        [gW1,gW2]=gradtarget(W1,W2,X_samples,Y_samples);
 
         w=packweight(W1,W2);
         dw=packweight(gW1,gW2);
